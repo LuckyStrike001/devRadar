989030d1f779fbdb4747a2d1913c7a9d00f133a5
Bruno Novais <bruno.br001@gmail.com>
2020-03-20 00:41:42 +0000

    app structure and style

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
   return (
@@ -30,7 +31,66 @@ function App() {
           <button type="submit">Save</button>
         </form>
       </aside>
-      <main></main>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
+                alt="Bruno Novais"
+              />
+              <div className="user-info">
+                <strong>Bruno Novais</strong>
+                <span>ReactJS, node.js</span>
+              </div>
+            </header>
+            <p>Tech lover</p>
+            <a href="https://github.com/LuckyStrike001">Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
+                alt="Bruno Novais"
+              />
+              <div className="user-info">
+                <strong>Bruno Novais</strong>
+                <span>ReactJS, node.js</span>
+              </div>
+            </header>
+            <p>Tech lover</p>
+            <a href="https://github.com/LuckyStrike001">Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
+                alt="Bruno Novais"
+              />
+              <div className="user-info">
+                <strong>Bruno Novais</strong>
+                <span>ReactJS, node.js</span>
+              </div>
+            </header>
+            <p>Tech lover</p>
+            <a href="https://github.com/LuckyStrike001">Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
+                alt="Bruno Novais"
+              />
+              <div className="user-info">
+                <strong>Bruno Novais</strong>
+                <span>ReactJS, node.js</span>
+              </div>
+            </header>
+            <p>Tech lover</p>
+            <a href="https://github.com/LuckyStrike001">Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }

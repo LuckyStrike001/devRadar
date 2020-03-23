f2e933c256d81d6e81d7aaf280c7d9f8c3cdbdb3
Bruno Novais <bruno.br001@gmail.com>
2020-03-23 00:51:07 +0000

    services and packages

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGitHubUsername] = useState("");
+  const [techs, setTechs] = useState("");
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
 
@@ -25,18 +31,56 @@ function App() {
     );
   }, []);
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGitHubUsername("");
+    setTechs("");
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Sign In</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Github Username</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGitHubUsername(e.target.value)}
+            />
           </div>
           <div className="input-block">
             <label htmlFor="techs">Techs</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
           <div className="input-group">
             <div className="input-block">
@@ -65,64 +109,22 @@ function App() {
           <button type="submit">Save</button>
         </form>
       </aside>
+
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
-                alt="Bruno Novais"
-              />
-              <div className="user-info">
-                <strong>Bruno Novais</strong>
-                <span>ReactJS, node.js</span>
-              </div>
-            </header>
-            <p>Tech lover</p>
-            <a href="https://github.com/LuckyStrike001">Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
-                alt="Bruno Novais"
-              />
-              <div className="user-info">
-                <strong>Bruno Novais</strong>
-                <span>ReactJS, node.js</span>
-              </div>
-            </header>
-            <p>Tech lover</p>
-            <a href="https://github.com/LuckyStrike001">Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
-                alt="Bruno Novais"
-              />
-              <div className="user-info">
-                <strong>Bruno Novais</strong>
-                <span>ReactJS, node.js</span>
-              </div>
-            </header>
-            <p>Tech lover</p>
-            <a href="https://github.com/LuckyStrike001">Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
-                alt="Bruno Novais"
-              />
-              <div className="user-info">
-                <strong>Bruno Novais</strong>
-                <span>ReactJS, node.js</span>
-              </div>
-            </header>
-            <p>Tech lover</p>
-            <a href="https://github.com/LuckyStrike001">Github</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(", ")}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${github_username}`}>Github</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>

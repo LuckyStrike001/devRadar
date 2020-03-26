9f18869d2f5c51bde800e536eafa577d328e9055
Bruno Novais <bruno.br001@gmail.com>
2020-03-26 00:55:02 +0000

    main - add map and mark

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,67 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
 
 function Main() {
-  return <View></View>;
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: 38.8565808, longitude: -9.0691528 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri:
+              "https://avatars3.githubusercontent.com/u/11559401?s=460&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
+          }}
+        />
+      </Marker>
+    </MapView>
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: "#FFF"
+  }
+});
 
 export default Main;
